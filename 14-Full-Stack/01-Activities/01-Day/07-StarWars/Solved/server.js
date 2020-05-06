7054ff297cfe7c0da1ffe456e1420917cd1fb707
logiccosmic@gmail.com <logiccosmic@gmail.com>
2020-05-05 17:29:32 -0700

    moved passport example out for a second;

--- 14-Full-Stack/01-Activities/01-Day/07-StarWars/Solved/server.js
+++ 14-Full-Stack/01-Activities/01-Day/07-StarWars/Solved/server.js
@@ -2,30 +2,15 @@
 // Server.js - This file is the initial starting point for the Node/Express server.
 // *********************************************************************************
 
-/*
- *  STEPS TO SEQUELIZE THE STAR WARS APP.
- *  1. Install the sequelize and mysql2 npm packages.
- *  2. Delete the orm from config. In your app folder, create a model folder
- *     with a character.js file in the model
- *  3. In character.js, model out the character table, as detailed
- *     in the schema.sql file located in the root of this project directory.
- *  4. Remove all references to the old orm file,
- *     and replace it with character.js
- *  5. Use Sequelize methods in place of the orm calls
- *     to retrieve and insert data.
- *  6. Update connection.js to use sequelize instead of the mysql package.
- *
- * -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/ */
-
 // Dependencies
 // =============================================================
-var express = require("express");
+const express = require("express");
 const db = require("./app/models")
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
--- 14-Full-Stack/01-Activities/01-Day/08-SequelizeLibrary/Solved/server.js
+++ 14-Full-Stack/01-Activities/01-Day/08-SequelizeLibrary/Solved/server.js
@@ -4,12 +4,13 @@
 
 // Dependencies
 // =============================================================
-var express = require("express");
+const express = require("express");
+const db = require("./app/models");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
